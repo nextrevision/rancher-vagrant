165ef2f928f9df6bb4459ffc8024ecfca96c08df
Josh Reichardt <josh.reichardt@gmail.com>
2016-05-27 11:49:45 -0700

    config: Make coreos version configurable

--- config_sample.rb
+++ config_sample.rb
@@ -2,9 +2,12 @@
 # $box = "rancherio/rancheros"
 # $box_version = '>=0.4.1'
 #
-# CoreOS can also be used instead of RancherOS
-# $box = "coreos-stable"
-# $box_url = "https://storage.googleapis.com/stable.release.core-os.net/amd64-usr/current/coreos_production_vagrant.json"
+# Official CoreOS channel to download from.  Either alpha, beta or stable
+#$update_channel = "alpha"
+# Change the version of CoreOS to deploy.  Use "current" if unsure
+#$image_version = "current"
+# URL to pull CoreOS image from
+#$box_url = "https://storage.googleapis.com/%s.release.core-os.net/amd64-usr/current/coreos_production_vagrant.json" % [$update_channel, $image_version]
 
 # Tag of the rancher/server image to run
 # $rancher_version = 'latest'
@@ -12,8 +15,7 @@
 # IP prefix to use when assigning box ip addresses
 # $ip_prefix = '192.168.33'
 
-# Enable syncing of the current directory
-# to the /vagrant path on the guest
+# Enable syncing of the current directory to the /vagrant path on the guest
 # $disable_folder_sync = false
 
 # Boxes to create in the vagrant environment
@@ -21,13 +23,13 @@ $boxes = [
     {
       "name"   => "rancher-server",
       "role"   => "server",
-      "memory" => "1536",
+      "memory" => "1024",
       "labels" => [],
     },
     {
       "name"   => "rancher-agent",
-      "count"  => 2,
+      "count"  => 1,
       "memory" => "512",
-      "labels" => ["type=general"]
+      "labels" => ["type=rancher-client"]
     },
 ]
